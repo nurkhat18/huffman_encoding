aa94238d1f162552d5e4205002ea06e5735b058f
Nurkhat Jumabaev <nurkhatjumabaev@arizona.edu>
2023-11-06 19:08:25 -0700

    changes

--- src/model/Leaf.java
+++ src/model/Leaf.java
@@ -1,5 +1,19 @@
 package model;
 
-public class Leaf {
+public class Leaf extends Node {
+	
+	private char letter;
+
+	public Leaf(int frequency, char letter) {
+		super(frequency);
+		this.letter = letter;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public char getLetter() {
+		return letter;
+	}
+	
+	
 
 }
--- src/model/Node.java
+++ src/model/Node.java
@@ -1,5 +1,38 @@
 package model;
 
-public class Node {
+public class Node  implements Comparable<Node>{
+	private Node leftNode;
+	private Node rightNode;
+	private int frequency;
+	
+	public Node(Node leftNode, Node rightNode) {
+		this.leftNode = leftNode;
+		this.rightNode = rightNode;
+		
+		this.frequency = leftNode.getFrequency() + rightNode.getFrequency();
+	}
+	
+	
+	public Node(int frequency) {
+		this.frequency = frequency;
+	}
 
+
+	public int getFrequency() {
+		return this.frequency;
+	}
+	
+	public Node getleftNode() {
+		return leftNode;
+	}
+	public Node getrightNode() {
+		return rightNode;
+	}
+
+
+	@Override
+	public int compareTo(Node node) {
+		// TODO Auto-generated method stub
+		return Integer.compare(frequency, node.getFrequency());
+	}
 }
