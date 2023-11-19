a364b602632612db7662c102a0003341487de9f6
Nurkhat Jumabaev <nurkhatjumabaev@arizona.edu>
2023-11-18 20:26:27 -0700

    finished

--- src/model/Huffman.java
+++ src/model/Huffman.java
@@ -7,6 +7,7 @@ public class Huffman {
 	private String text;
 	private HashMap<Character, Integer> charFrequencies;
 	private HashMap<Character, String> huffmanCodes;
+	private Node root;
 	
 	
 	public Huffman(String text) {
@@ -41,6 +42,7 @@ public class Huffman {
 		while(queue.size() > 1) {
 			queue.enqueue(new Node(queue.dequeue(), queue.dequeue()));
 		}
+		root = queue.dequeue();
 	}
 	
 	private void generateHuffmanCodes(Node node, String code) {
@@ -50,6 +52,35 @@ public class Huffman {
 		
 		generateHuffmanCodes(node.getLeftNode(), code.concat("0"));
 		generateHuffmanCodes(node.getRightNode(), code.concat("1"));
+	}
+	
+	private String getEncodedText() {
+		StringBuilder string = new StringBuilder();
+		for (char character: text.toCharArray()) {
+			string.append(huffmanCodes.get(character));
+		}
+		
+		return string.toString();
+	}
+	
+	private String decode(String encodedText) {
+		StringBuilder sb = new StringBuilder();
+		Node current = root;
+		for (char character: encodedText.toCharArray()) {
+			if (character == '0') {
+				current = current.getLeftNode();
+			}
+			else {
+				current = current.getRightNode();
+			}
+			
+			if (current instanceof Leaf) {
+				sb.append(((Leaf) current).getLetter());
+				current = root;
+			}
+			
+		}
+		return sb.toString();
 	}
 	
 }
