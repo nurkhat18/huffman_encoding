79db5a89259d9fe22958691b2195d0bb687ad818
Prezynae <126417725+Prezynae@users.noreply.github.com>
2023-11-21 19:54:29 -0700

    Create HuffmanTest.java

---
+++ src/test/HuffmanTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import model.Huffman;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HuffmanTest {
+
+    private Huffman huffman;
+
+    @BeforeEach
+    //setup to correctly test the huffman class
+    void setup() {
+        huffman = new Huffman("sample code to test huffman class");
+    }
+
+    @Test
+    //testing the encoding method of huffman class
+    void testEncoding() {
+        String encoded = huffman.getEncodedText();
+        assertNotNull(encoded);
+            }
+
+    @Test
+    //testing the decoding method of huffman class
+    void testDecoding() {
+        String encoded = huffman.getEncodedText();
+        String decoded = huffman.decode(encoded);
+        assertEquals("sample code to test huffman class", decoded);
+    }
+
+
+}
