87f18a2c0ea6da28d9b924d328242b3b6be8acbd
Prezynae <126417725+Prezynae@users.noreply.github.com>
2023-11-21 19:58:34 -0700

    Create AccountTest.java

---
+++ src/application/AccountTest.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    //Testing whether an account can be created or not
+    void testAccountCreation() {
+        account acc = new account("user1", "pass123");
+        assertNotNull(acc);
+        assertEquals("user1", acc.getUserName());
+        assertEquals("pass123", acc.getPassword());
+    }
+
+    @Test
+    //Testing the observable list of an account which holds messages
+    void testObservableList() {
+        account acc = new account("user2", "pass456");
+        ObservableList<text> list = acc.returnList();
+        assertNotNull(list);
+        assertTrue(list.isEmpty());
+    }
+
+}
