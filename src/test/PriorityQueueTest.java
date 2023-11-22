99cb89b3371f1dfb4491d857aced21f9a64f7fe5
Prezynae <126417725+Prezynae@users.noreply.github.com>
2023-11-21 19:56:47 -0700

    Create PriorityQueueTest.java

---
+++ src/test/PriorityQueueTest.java
@@ -0,0 +1,45 @@
+package test;
+import org.junit.Test;
+
+import model.Leaf;
+import model.PriorityQueue;
+
+import static org.junit.Assert.*;
+
+public class PriorityQueueTest {
+
+    @Test
+    //testing priority queue enqueue
+    public void testEnqueueAndPeek() {
+        PriorityQueue priorityQueue = new PriorityQueue(5);
+        priorityQueue.enqueue(new Leaf('A', 2));
+        priorityQueue.enqueue(new Leaf('B', 4));
+        priorityQueue.enqueue(new Leaf('C', 4));
+        Leaf l = (Leaf) priorityQueue.peek();
+        assertEquals('B', l.getLetter()); 
+    }
+
+    @Test
+    //testing dequeue
+    public void testDequeue() {
+        PriorityQueue priorityQueue = new PriorityQueue(5);
+        priorityQueue.enqueue(new Leaf('A', 2));
+        priorityQueue.enqueue(new Leaf('B', 4));
+        priorityQueue.enqueue(new Leaf('C', 4));
+        priorityQueue.dequeue();
+        Leaf l= (Leaf) priorityQueue.peek();
+        assertEquals('C', l.getLetter());  
+    }
+
+    @Test
+    //testing an empty queue
+    public void testEmptyQueue() {
+        PriorityQueue priorityQueue = new PriorityQueue(5);
+        assertTrue(priorityQueue.isEmpty()); 
+        priorityQueue.enqueue(new Leaf('A', 2));
+        priorityQueue.dequeue();
+        assertTrue(priorityQueue.isEmpty()); 
+    }
+
+}
+
